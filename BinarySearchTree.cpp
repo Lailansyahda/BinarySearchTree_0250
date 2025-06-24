293be8f6fea4f5eed4d33cd5a40b432a0133ea07
Lailansyahda <syahdaazalia123@gmail.com>
2025-06-24 10:19:20 +0700

    make the left and right child of the new node point to NULL

--- BinarySearchTree.cpp
+++ BinarySearchTree.cpp
@@ -37,5 +37,9 @@ class BinaryTree
 
         //step 2: assign value to the data field of new node
         newNode->info = x;
+
+        //step 3: make the left and right child of the new node point to NULL
+        newNode->leftchild = nullptr;
+        newNode->rightchild = nullptr;
     }
 }
