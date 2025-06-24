982dd4de192833de1a149cbc621d94119a0882ef
Lailansyahda <syahdaazalia123@gmail.com>
2025-06-24 10:33:28 +0700

    prosedur search

--- BinarySearchTree.cpp
+++ BinarySearchTree.cpp
@@ -75,6 +75,22 @@ class BinaryTree
             //7b : Exit
             return;
         }
-        
     }
+
+    void search(int element, Node *&parent, Node *&currentNode)
+    {
+        //This function searches the currentNode of the specified Node as well as the current Node of its parent
+        currentNode = ROOT;
+        parent = nullptr;
+        while ((currentNode != nullptr) && (currentNode->info != element))
+        {
+            parent = currentNode;
+            if (element < currentNode->info)
+                currentNode = currentNode->leftchild;
+            else   
+                currentNode = currentNode->rightchild;
+        }
+    }
+
+    
 }
